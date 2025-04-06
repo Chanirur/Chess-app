b362c45aa42a30ab411831a835ea4232165820e8
Chaniru Ranasinghe <chaniru.ranasinghe123@gmail.com>
2025-04-06 18:47:41 +0000

    websocket

---
+++ ws-server/src/server.js
@@ -0,0 +1,44 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const ws_1 = require("ws");
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+function onSocketPreError(e) {
+    console.log(e);
+}
+function onSocketPostError(e) {
+    console.log(e);
+}
+console.log(`Attempting to run server on port ${port}`);
+const s = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+const wss = new ws_1.WebSocketServer({ noServer: true });
+s.on('upgrade', (req, socket, head) => {
+    socket.on('error', onSocketPreError);
+    // perform auth
+    if (!!req.headers['BadAuth']) {
+        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+        socket.destroy();
+        return;
+    }
+    wss.handleUpgrade(req, socket, head, (ws) => {
+        socket.removeListener('error', onSocketPreError);
+        wss.emit('connection', ws, req);
+    });
+});
+wss.on('connection', (ws, req) => {
+    ws.on('error', onSocketPostError);
+    ws.on('message', (msg, isBinary) => {
+        wss.clients.forEach((client) => {
+            client.send(msg, { binary: isBinary });
+        });
+    });
+    ws.on('close', () => {
+        console.log('Connection closed');
+    });
+});
--- ws-server/src/server.ts
+++ ws-server/src/server.ts
@@ -1,18 +1,51 @@
-import * as WebSocket from "ws";
+import express from 'express';
+import { WebSocketServer, WebSocket } from 'ws';
 
-const wss = new WebSocket.Server({ port: 8080 });
+const app = express();
+const port = process.env.PORT || 3000;
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
+function onSocketPreError(e: Error) {
+    console.log(e);
+}
 
-    ws.on('message', (message) => {
-        console.log(`Received: ${message}`);
-        ws.send(`Server received: ${message}`);
-    });
+function onSocketPostError(e: Error) {
+    console.log(e);
+}
 
-    ws.on('close', () => {
-        console.log('Client disconnected');
+console.log(`Attempting to run server on port ${port}`);
+
+const s = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+const wss = new WebSocketServer({ noServer: true });
+
+s.on('upgrade', (req, socket, head) => {
+    socket.on('error', onSocketPreError);
+
+    // perform auth
+    if (!!req.headers['BadAuth']) {
+        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+        socket.destroy();
+        return;
+    }
+
+    wss.handleUpgrade(req, socket, head, (ws) => {
+        socket.removeListener('error', onSocketPreError);
+        wss.emit('connection', ws, req);
     });
 });
 
-console.log('WebSocket server running on port 8080');
+wss.on('connection', (ws, req) => {
+    ws.on('error', onSocketPostError);
+
+    ws.on('message', (msg, isBinary) => {
+        wss.clients.forEach((client) => {
+            client.send(msg, { binary: isBinary });
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Connection closed');
+    });
+});
